f0cbaca6357734cce6b02a8269809634686d4ae9
chegueidelivery69-rgb <chegueidelivery69@gmail.com>
2026-02-06 07:25:44 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import '../../../app/app.dart';
 import '../../../common/common.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await commonSetup();
+
   runApp(const MyApp());
 }
